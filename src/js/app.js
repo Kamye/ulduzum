70b1d4846e0c0470d2f720286616b13de001819a
Kamye <andrej15ganzha@gmail.com>
2017-02-02 02:20:51 +0200

    made rules and works

--- src/js/app.js
+++ src/js/app.js
@@ -20,3 +20,23 @@ $('.footer-number__area').on('input', function(){
         $(this).parent().find('.footer-number__call').removeClass('opacity1');
     }
 })
+
+$(function($) {
+    var allAccordions = $('.accordion div.data');
+    var allAccordionItems = $('.accordion .accordion-item');
+    $('.accordion > .accordion-item').click(function() {
+        if($(this).hasClass('open'))
+        {
+            $(this).removeClass('open');
+            $(this).next().slideUp("slow");
+        }
+        else
+        {
+            allAccordions.slideUp("slow");
+            allAccordionItems.removeClass('open');
+            $(this).addClass('open');
+            $(this).next().slideDown("slow");
+            return false;
+        }
+    });
+});
