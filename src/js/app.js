3d0282abb34c537c57325bafcb817e0e3d55c112
wonderlaw1 <wonderlaw12@gmail.com>
2017-03-21 17:37:20 +0200

    fix menu and filter

--- src/js/app.js
+++ src/js/app.js
@@ -153,7 +153,7 @@ $(function(){
     $('#nav').slicknav({
         'afterOpen': function () {
             $('html, body').animate({scrollTop: 0}, 100);
-            $('html').css('position', 'fixed');
+            $('html, body').css('position', 'fixed');
             var win_height = $(window).height();
             $('.slicknav_nav').height(win_height - 132);
             $('body').css('overflow', 'hidden');
@@ -161,7 +161,7 @@ $(function(){
             $('.slicknav_menu').append('<a class="nav-item__link nav-item__link_sms nav-item__link_active" href="#" role="menuitem" tabindex="0"><span>Pulsuz SMS</span></a>')
         },
         'beforeClose': function () {
-            $('html').css('position', 'static');
+            $('html, body').css('position', 'static');
             $('.slicknav_menu .nav-item__link_sms').remove();
             $('body').css('overflow', 'auto');
         }
@@ -215,7 +215,7 @@ $('.show-filter').on('click', function () {
     $('html, body').animate({scrollTop: 0}, 100);
     $('.sort-modal, .sort-backdrop').show();
     $('body').addClass('body-show-filter');
-    $('html').css('position', 'fixed');
+    $('html, body').css('position', 'fixed');
     setTimeout(function () {
         var height_filter = $('.sort-dialog').height();
         $('.sort-filter-wrap').height(height_filter - 101);
@@ -226,7 +226,7 @@ $(window).on('resize', function () {
     if ($('.sort-modal').length > 0 ) {
         var height_filter = $('.sort-dialog').height();
         $('.sort-filter-wrap').height(height_filter - 101);
-        
+
         var win_height = $(window).height();
         $('.slicknav_nav').height(win_height - 132);
         $('body').css('overflow', 'hidden');
@@ -236,7 +236,7 @@ $(window).on('resize', function () {
 $('.sort-close__btn').on('click', function(){
     $('.sort-modal, .sort-backdrop').hide();
     $('body').removeClass('body-show-filter');
-    $('html').css('position', 'static');
+    $('html, body').css('position', 'static');
 });
 
 $('.sort-filter__header').on('click', function () {
